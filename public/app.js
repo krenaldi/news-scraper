c48860a70669153b3c37f92351bc84ec23e59488
Kris Renaldi <krenaldi@comcast.net>
2019-10-08 00:03:20 -0400

    Changed app.js to have articles listed in cards

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON('/articles', function (data) {
     // For each article
     for (let i = 0; i < data.length; i++) {
         // Display the articles on the page
-        $('#articles').append("<p data-id='" + data[i]._id + "'><a href='" + data[i].link + "' target='_blank'>" + data[i].title + "</a><br />" + data[i].intro + "</p>");
+        $('#articles').append(`<div class="card"><div class="card-body"><h5 class="card-title" data-id="${data[i]._id}"><a href=${data[i].link}" target="_blank">${data[i].title}</a></h5><p data-id="${data[i]._id}">${data[i].intro}</p></div></div>`);
     }
 });
 
@@ -13,7 +13,7 @@ $(document).on("click", "p", function() {
     $("#notes").empty();
     // Save the id from the p tag
     var thisId = $(this).attr("data-id");
-    console.log(thisId);
+    //console.log(thisId);
     // Now make an ajax call for the Article
     $.ajax({
         method: "GET",
@@ -24,12 +24,10 @@ $(document).on("click", "p", function() {
             console.log(data);
             // The title of the article
             $("#notes").append("<strong>" + data.title + "</strong>");
-            // An input to enter a new title
-            // $("#notes").append("<input id='titleinput' name='title' >");
             // A textarea to add a new note body
             $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
             // A button to submit a new note, with the id of the article saved to it
-            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+            $("#notes").append("<button class='btn btn-primary' data-id='" + data._id + "' id='savenote'>Add Note</button>");
 
             // If there's a note in the article
             if (data.note) {
